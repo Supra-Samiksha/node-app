9adb421662a703245844b9ba2a1198485409b967
Supra-Samiksha <supradkl06@gmail.com>
2026-07-20 20:25:01 +0530

    Trial 3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>GitOps CI/CD Automation Success!!!</h1>');
+    res.send('<h1>GitOps CI/CD Automation Success!</h1>');
 });
 
 app.listen(PORT, () => {
